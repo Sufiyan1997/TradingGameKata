b9139f5fbce706e39a32846b0743dc97f7bd622a
Sufiyan Gani <sufigani@gmail.com>
2020-07-22 00:20:54 +0530

    PT: for player initialization

--- src/main/java/org/kata/Player.java
+++ src/main/java/org/kata/Player.java
@@ -1,28 +1,41 @@
 package org.kata;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Player {
+    int mana;
+    int manaSlots;
+    int health;
+    String name;
+    List<Integer> deck;
+
     public Player(String name) {
+        this.mana = 0;
+        this.manaSlots = 0;
+        this.health = 30;
+        this.name = name;
+        this.deck = new ArrayList<Integer>(Arrays.asList(0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8));
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public int getMana() {
-        return -1;
+        return mana;
     }
 
     public int getManaSlots() {
-        return -1;
+        return manaSlots;
     }
 
     public int getHealth() {
-        return -1;
+        return health;
     }
 
     public List<Integer> getDeck() {
-        return null;
+        return deck;
     }
 }
