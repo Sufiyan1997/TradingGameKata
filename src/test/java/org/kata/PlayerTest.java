75cbde9e0b0c868fb94fe3091d781bbfd917aae7
Sufiyan Gani <sufigani@gmail.com>
2020-07-24 15:17:16 +0530

    FT: for cannot draw card if deck is full

--- src/test/java/org/kata/PlayerTest.java
+++ src/test/java/org/kata/PlayerTest.java
@@ -28,4 +28,20 @@ public class PlayerTest {
         Player player = new Player(0, 0, 30, "abc", initialDeck, initialHand);
         assertFalse(player.canDrawCard());
     }
+
+    @Test
+    public void playerCannotDrawCardIfDeckEmptyTest() {
+        List<Integer> initialDeck = new ArrayList<Integer>(new ArrayList<Integer>());
+        List<Integer> initialHand = new ArrayList<Integer>(Arrays.asList(1,2,3,4));
+        Player player = new Player(0, 0, 30, "abc", initialDeck, initialHand);
+        assertFalse(player.canDrawCard());
+    }
+
+    @Test
+    public void playerCanDrawIfHandNotFullAndDeckNotEmpty() {
+        List<Integer> initialDeck = new ArrayList<Integer>(Arrays.asList(0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8));
+        List<Integer> initialHand = new ArrayList<Integer>(new ArrayList<Integer>());
+        Player player = new Player(0, 0, 30, "abc", initialDeck, initialHand);
+        assertTrue(player.canDrawCard());
+    }
 }
