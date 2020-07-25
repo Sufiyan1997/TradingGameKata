9a18874b951de82426b5d7e9a69850f10c37d9e7
Sufiyan Gani <sufigani@gmail.com>
2020-07-25 12:04:54 +0530

    PT: to check continuous polling for input till correct input is provided

--- src/main/java/org/kata/UI/ConsoleUI.java
+++ src/main/java/org/kata/UI/ConsoleUI.java
@@ -1,6 +1,11 @@
 package org.kata.UI;
 
+import java.util.Scanner;
+
 public class ConsoleUI implements UI{
+
+    private Scanner scanner = new Scanner(System.in);
+
     @Override
     public void message(String msg) {
         System.out.println(msg);
@@ -8,6 +13,15 @@ public class ConsoleUI implements UI{
 
     @Override
     public int getMove() {
-        return 0;
+        while (true) {
+            String line = scanner.nextLine();
+            try {
+                int move = Integer.parseInt(line);
+                return move;
+            }
+            catch (NumberFormatException e) {
+                message("Invalid input");
+            }
+        }
     }
 }
