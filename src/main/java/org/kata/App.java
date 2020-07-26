bb5ad076b47a91c99814c5114c33c11ada939fd0
Sufiyan Gani <sufigani@gmail.com>
2020-07-26 12:18:28 +0530

    FT: to check active player is set

--- src/main/java/org/kata/App.java
+++ src/main/java/org/kata/App.java
@@ -9,11 +9,19 @@ import org.kata.UI.UI;
 public class App 
 {
     private UI ui;
-    public App(Player p1, Player p2, UI ui) {
+    public App(Player p1, Player p2, int initialPlayer, UI ui) {
         this.ui = ui;
 
         p1.pickInitialHand();
         p2.pickInitialHand();
+    }
+
+    public Player getActivePlayer() {
+        return null;
+    }
+
+    public Player getInactivePlayer() {
+        return null;
     }
 
     public static void main( String[] args )
--- src/test/java/org/kata/AppTest.java
+++ src/test/java/org/kata/AppTest.java
@@ -4,6 +4,7 @@ package org.kata;
 import org.junit.Test;
 import org.kata.UI.UI;
 import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
 
 /**
  * Unit test for simple App.
@@ -15,9 +16,20 @@ public class AppTest
         Player p1 = mock(Player.class);
         Player p2 = mock(Player.class);
         UI ui = mock(UI.class);
-        App app = new App(p1, p2, ui);
+        App app = new App(p1, p2, 1, ui);
 
         verify(p1).pickInitialHand();
         verify(p2).pickInitialHand();
+    }
+
+    @Test
+    public void setActivePLayerTest() {
+        Player p1 = mock(Player.class);
+        Player p2 = mock(Player.class);
+        UI ui = mock(UI.class);
+        App app = new App(p1, p2, 1, ui);
+
+        assertEquals(app.getActivePlayer(), p1);
+        assertEquals(app.getInactivePlayer(), p2);
     }
 }
