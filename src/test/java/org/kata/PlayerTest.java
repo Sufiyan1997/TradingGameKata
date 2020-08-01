3c93712fe61fc98330d61a9a73d23fe3ba5f4ec3
Sufiyan Gani <sufigani@gmail.com>
2020-08-01 15:08:26 +0530

    OTHER: removed tests invalidated by special rule of overload and bleed out

--- src/test/java/org/kata/PlayerTest.java
+++ src/test/java/org/kata/PlayerTest.java
@@ -22,21 +22,6 @@ public class PlayerTest {
         assertTrue(player.getDeck().equals(initialDeck));
     }
 
-    @Test
-    public void playerCannotDrawCardIfHandfullTest() {
-        List<Integer> initialDeck = new ArrayList<Integer>(Arrays.asList(0,0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,7,8));
-        List<Integer> initialHand = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5));
-        Player player = new Player(0, 0, 30, "abc", initialDeck, initialHand);
-        assertFalse(player.canDrawCard());
-    }
-
-    @Test
-    public void playerCannotDrawCardIfDeckEmptyTest() {
-        List<Integer> initialDeck = new ArrayList<Integer>(new ArrayList<Integer>());
-        List<Integer> initialHand = new ArrayList<Integer>(Arrays.asList(1,2,3,4));
-        Player player = new Player(0, 0, 30, "abc", initialDeck, initialHand);
-        assertFalse(player.canDrawCard());
-    }
 
     @Test
     public void playerCanDrawIfHandNotFullAndDeckNotEmpty() {
